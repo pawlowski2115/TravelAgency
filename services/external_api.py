55a85eb4ad10d85e32b305797ecb02b9fe4b156f
Kamil Pawlowski <pawlowski.kamil.to@gmail.com>
2026-06-06 21:26:31 +0300

    external api

---
+++ services/external_api.py
@@ -0,0 +1,48 @@
+import requests
+
+
+WEATHER_API_KEY = "22e007806268489f3af8430a0d0a3301"
+
+def get_current_weather(city_name):
+    try:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={WEATHER_API_KEY}&units=metric"
+        response = requests.get(url, timeout=5)
+
+        if response.status_code == 200:
+            data = response.json()
+            temp = round(data['main']['temp'])
+            icon = data['weather'][0]['icon']
+
+            return {"temp": temp, "icon": icon}
+        else:
+            return {"temp": 20, "icon": "01d"}
+    except requests.RequestException:
+        return {"temp": 18, "icon": "02d"}
+
+def get_current_weather(city_name):
+    return None
+
+def get_currency_rate(currency_code):
+    if currency_code == "PLN":
+        return 1.0
+    try:
+        url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency_code}/?format=json"
+        response = requests.get(url, timeout=5)
+
+        if response.status_code == 200:
+            data = response.json()
+            rate = data['rates'][0]['mid']
+            return float(rate)
+        else:
+            return get_fallback_rate(currency_code)
+        
+    except:
+            return get_fallback_rate(currency_code)
+    
+def get_fallback_rate(currency_code):
+    rates = {
+          "EUR": 4.30,
+          "USD": 4.00,
+          "JPY": 0.026
+    }
+    return rates.get(currency_code, 1.0)
